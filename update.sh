efe34d684be8dded3a304e5af1bad44a798a55a5
aderama2711 <ade.ramadha@gmail.com>
2022-05-20 14:53:46 +0700

    fix update script

--- update.sh
+++ update.sh
@@ -3,21 +3,21 @@ cd ndn-cxx
 git pull
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 cd ..
 cd nfd
 git pull
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 cd ..
 cd psync
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 cd ..
 cd nlsr
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 cd ..
