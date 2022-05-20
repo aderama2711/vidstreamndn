b8ec894cc1e326e86648398a4186a991b07d8097
aderama2711 <ade.ramadha@gmail.com>
2022-05-20 13:36:33 +0700

    add update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+cd ndn-cxx
+git pull
+.waf configure
+.waf
+.waf install
+cd ..
+cd nfd
+git pull
+.waf configure
+.waf
+.waf install
+cd ..
+cd psync
+.waf configure
+.waf
+.waf install
+cd ..
+cd nlsr
+.waf configure
+.waf
+.waf install
+cd ..
